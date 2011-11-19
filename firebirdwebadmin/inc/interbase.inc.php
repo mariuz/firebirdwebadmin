8701e5f05418f57c614ea23a557b048541f82d93
Popa Adrian Marius <mapopa@gmail.com>
2011-11-19 12:24:10 +0200

    fix unpaired paranthesis

--- firebirdwebadmin/inc/interbase.inc.php
+++ firebirdwebadmin/inc/interbase.inc.php
@@ -1,15 +1,4 @@
 <?php
-// File           interbase.inc.php / ibWebAdmin
-// Purpose        provides the Interbase constants
-// Author         Lutz Brueckner <irie@gmx.de>
-// Copyright      (c) 2000-2006 by Lutz Brueckner,
-//                published under the terms of the GNU General Public Licence v.2,
-//                see file LICENCE for details
-// Created        <00/09/09 18:41:57 lb>
-//
-// $Id: interbase.inc.php,v 1.21 2006/03/12 21:43:24 lbrueckner Exp $
-
-
 // only used on th login panel while $s_charsets is empty
 function get_preset_charsets($server_family=NULL, $server_version=NULL) {
 
@@ -247,7 +236,6 @@ function get_system_tables($server_family, $server_version) {
               'RDB$USER_PRIVILEGES',
               'RDB$VIEW_RELATIONS');
 
-    }
 
     return $system_tables;
 }
