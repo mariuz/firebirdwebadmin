aee1430c9247d329f85f804f1df7561e0e9d8b32
Popa Adrian Marius <mapopa@gmail.com>
2014-02-25 15:01:28 +0200

    cleanup

--- inc/display_variable.php
+++ inc/display_variable.php
@@ -5,9 +5,6 @@
 // Copyright      (c) 2000, 2001, 2002, 2003, 2004 by Lutz Brueckner,
 //                published under the terms of the GNU General Public Licence v.2,
 //                see file LICENCE for details
-// Created        <02/10/14 17:47:02 lb>
-//
-// $Id: display_variable.php,v 1.3 2004/10/24 16:33:43 lbrueckner Exp $
 
 require('./configuration.inc.php');
 require('./debug_funcs.inc.php');
