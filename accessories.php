e0d42606335418ea5746afcb00eb466d13abd952
Popa Marius Adrian <mapopa@gmail.com>
2014-04-07 19:34:44 +0300

    Update accessories.php

--- accessories.php
+++ accessories.php
@@ -1,14 +1,10 @@
 <?php
-// File           accessories.php / FirebirdWebAdmin
 // Purpose        working with generators, triggers, domains, indexes, views,
 //                stored procedures, user defined functions and exceptions
 // Author         Lutz Brueckner <irie@gmx.de>
 // Copyright      (c) 2000-2006 by Lutz Brueckner,
 //                published under the terms of the GNU General Public Licence v.2,
 //                see file LICENCE for details
-// Created        <00/10/02 08:47:06 lb>
-
-
 
 require('./inc/script_start.inc.php');
 
