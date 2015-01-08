fbd373c5f931ad6e533effe62e33545bbba2187f
Popa Adrian Marius <mapopa@reea.net>
2015-01-08 17:41:20 +0200

    Revert "cleanup"
    This reverts commit acda53d586f3e642b545f0b604d35d6e54f57a60.

--- inc/xml_http_request_server.php
+++ inc/xml_http_request_server.php
@@ -102,7 +102,7 @@ function column_config_form($fk_table, $table, $column) {
           . '<input type="submit" name="dt_column_config_save" value="'.$button_strings['Save']."\" class=\"bgrp\">\n"
           . '<input type="button" name="dt_column_config_cancel" onClick="javascript:hide(this.parentNode.id);" value="'.$button_strings['Cancel']."\" class=\"bgrp\">\n";
 
-	header('Content-Type: text/html;charset=utf-8');
+    header('Content-Type: text/html;charset='.$GLOBALS['charset']);
 
     echo $html;
 }
@@ -121,8 +121,13 @@ function closed_panel($idx){
     $icon = get_icon_path(DATAPATH, ICON_SIZE) . 'open.png';
     $html = get_closed_panel($GLOBALS[$pvar][$idx][1], $idx, $icon);
 
+    // work around for a Mozilla bug 
+    if ($GLOBALS['s_useragent']['ns6up'] == TRUE) {
+        $html = str_replace('&amp;', '&', $html);
+    }
+
     set_customize_cookie($GLOBALS['s_cust']);
-    header('Content-Type: text/html;charset=utf-8');
+    header('Content-Type: text/html;charset='.$GLOBALS['charset']);
 
     echo $html;
 }
