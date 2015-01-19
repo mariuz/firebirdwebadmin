eba646f37eebc903244b53328ea1e7a43fd5961e
Popa Marius Adrian <mapopa@gmail.com>
2015-01-19 11:16:18 +0200

    Update export.inc.php

--- inc/export.inc.php
+++ inc/export.inc.php
@@ -1,12 +1,9 @@
 <?php
-// File           export.inc.php / FirebirdWebAdmin
 // Purpose        functions for the data export panel
 // Author         Lutz Brueckner <irie@gmx.de>
 // Copyright      (c) 2000-2006 by Lutz Brueckner,
 //                published under the terms of the GNU General Public Licence v.2,
 //                see file LICENCE for details
-// Created        <05/10/02 14:48:31 lb>
-
 
 
 //
