8f33039ab5a71edf4873ea6bec99549e4aef5fdb
Popa Marius Adrian <mapopa@gmail.com>
2015-01-19 12:26:13 +0200

    Update markableTable.js

--- js/markableTable.js
+++ js/markableTable.js
@@ -1,12 +1,9 @@
-// File           js/markableTable.js / FirebirdWebAdmin
 // Purpose        mark rows of a table with mouse clicks
 // Author         Lutz Brueckner <irie@gmx.de>
 // Copyright      (c) 2000-2006 by Lutz Brueckner,
 //                published under the terms of the GNU General Public Licence v.2,
 //                see file LICENCE for details
-// Created        <19/02/061 22:44:30 lb>
-//
-// $Id: markableTable.js,v 1.2 2006/07/08 17:13:44 lbrueckner Exp $
+
 
 
 /*
