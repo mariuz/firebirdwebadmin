bbcbac9d1f81c10bd333c1e3b734110d6b99cae6
Popa Marius Adrian <mapopa@gmail.com>
2015-01-20 16:14:29 +0200

    Update showblob.php

--- showblob.php
+++ showblob.php
@@ -1,13 +1,10 @@
 <?php
-// File           showblob.php / FirebirdWebAdmin
 // Purpose        displays the blob specified through the GET-varibles
 // Author         Lutz Brueckner <irie@gmx.de>
 // Copyright      (c) 2000, 2001, 2002, 2003, 2004, 2005 by Lutz Brueckner,
 //                published under the terms of the GNU General Public Licence v.2,
 //                see file LICENCE for details
-// Created        <01/10/21 13:25:50 lb>
-//
-// $Id: showblob.php,v 1.21 2005/09/25 17:51:59 lbrueckner Exp $
+
 
 
 // GET-Variables specifying the blob to display:
