5b41cc17f619f95afba27369aa06523e0671fd4d
Popa Adrian Marius <mapopa@gmail.com>
2015-11-24 15:35:39 +0200

    Cleanup

--- inc/array_functions.inc.php
+++ inc/array_functions.inc.php
@@ -5,10 +5,6 @@
 // Copyright      (c) 2000-2006 by Lutz Brueckner,
 //                published under the terms of the GNU General Public Licence v.2,
 //                see file LICENCE for details
-// Created        <00/10/23 12:10:07 lb>
-//
-// $Id: array_functions.inc.php,v 1.6 2006/03/22 21:05:27 lbrueckner Exp $
-
 
 //
 // swap the elements $from and $to in $arr, return the array
